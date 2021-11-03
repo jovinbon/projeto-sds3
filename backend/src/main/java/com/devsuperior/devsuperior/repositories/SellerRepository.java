b6cfcfa2c062386189630570f17eb317eee56aa0
Cristiano Fontes <jovinbon@hotmail.com>
2021-11-03 15:54:48 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/devsuperior/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.devsuperior.repositories;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.devsuperior.devsuperior.entities.Seller;
+
+public interface SellerRepository extends JpaRepositoryImplementation<Seller, Long> {
+
+}
